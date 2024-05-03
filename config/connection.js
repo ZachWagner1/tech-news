f2788d99fb754b02ff025abd9ff98dd5d82d6cbc
Zachary Wagner <zwag14@yahoo.com>
2024-05-02 22:04:22 -0500

    api routes for user model

--- config/connection.js
+++ config/connection.js
@@ -1,9 +1,9 @@
 const Sequelize = require('sequelize');
 
-require('dotenv'),config();
+require('dotenv').config();
 
 // create connection to db
-const sequelize = new Sequelize('tech_news_db', 'username', 'passowrd', {
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
     host: 'localhost',
     dialect: 'mysql',
     port: 3306
